3b0803e87c014c0f9ef343087223cf63bbc2c222
Jason Yu <jason.yu@askattest.com>
2020-06-05 16:48:10 +0100

    kkkkk

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="fine">
+    fine
+    <div class="fine__not">
+      not fine
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="postcss" scoped>
+.fine {
+  color: green;
+
+  &__not {
+    color: red;
+  }
+}
+</style>
